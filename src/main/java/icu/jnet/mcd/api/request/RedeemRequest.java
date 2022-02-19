759c22eef3005c5e87172391ec9c2eeeae15f212
JicuNull <69324615+JicuNull@users.noreply.github.com>
2022-02-19 12:44:33 +0100

    Revert "Fix problem with offerId"
    This reverts commit b92d073c4e0f7c923ac6a742493a29044ad0db75.

--- src/main/java/icu/jnet/mcd/api/request/RedeemRequest.java
+++ src/main/java/icu/jnet/mcd/api/request/RedeemRequest.java
@@ -12,6 +12,6 @@ public class RedeemRequest implements Request {
     @Override
     public String getUrl() {
         String url = "https://eu-prod.api.mcd.com/exp/v1/offers/redemption/" + propositionId;
-        return url + (!offerId.equals("0") ? "?offerId=" + offerId : "");
+        return url + (offerId != null ? "?offerId=" + offerId : "");
     }
 }
