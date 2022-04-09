40436cab81321e151ddad70bf3edd883a16193ae
JicuNull <69324615+JicuNull@users.noreply.github.com>
2022-04-09 23:29:32 +0200

    Add preferences to ProfileRequest

--- src/main/java/icu/jnet/mcd/api/request/ProfileRequest.java
+++ src/main/java/icu/jnet/mcd/api/request/ProfileRequest.java
@@ -3,7 +3,9 @@ package icu.jnet.mcd.api.request;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProfileRequest implements Request {
 
@@ -30,22 +32,47 @@ public class ProfileRequest implements Request {
 
         private final Base base = new Base();
         private final Audit audit = new Audit();
-        private List<Address> address;
-        private List<Subscription> subscriptions;
+        private Address[] address;
+        private Subscription[] subscriptions;
+        private Preference[] preferences;
         private Device[] devices;
+        private final Email[] email = new Email[] {new Email()};
+        private final Object[] phone = new Object[0];
 
         private void setZipCode(String zipCode) {
-            address = new ArrayList<>();
-            address.add(new Address(zipCode));
+            address = new Address[] {new Address(zipCode)};
         }
 
         private void useMyMcDonalds(boolean b, String deviceId) {
             String time = Instant.now().truncatedTo(ChronoUnit.SECONDS).toString().replace("Z", "");
-            subscriptions = new ArrayList<>();
-            subscriptions.add(new Subscription(time, b ? "N" : "Y", "Secondary Processing", "21"));
-            subscriptions.add(new Subscription(time, b ? "Y" : "N", "Personal Marketing", "23"));
-            subscriptions.add(new Subscription(time, b ? "Y" : "N", "Loyalty Enrollment", "24"));
-            subscriptions.add(new Subscription(time, b ? "Y" : "N", "Loyalty Communication", "25"));
+            subscriptions = new Subscription[] {
+                    new Subscription(time, "Y", "CommunicationChannel", "1"),
+                    new Subscription(time, "Y", "Surveys", "2"),
+                    new Subscription(time, "Y", "ProgramChanges", "3"),
+                    new Subscription(time, "Y", "Contests", "4"),
+                    new Subscription(time, "Y", "OtherMarketingMessages", "5"),
+                    new Subscription(time, "Y", "OfferProgram", "7"),
+                    new Subscription(time, "N", "EmailNotificationEnabled", "10"),
+                    new Subscription(time, "N", "MobileNotificationEnabled", "11"),
+                    new Subscription(time, b ? "N" : "Y", "Secondary Processing", "21"),
+                    new Subscription(time, "Y", "General Marketing", "22"),
+                    new Subscription(time, b ? "Y" : "N", "Personal Marketing", "23"),
+                    new Subscription(time, b ? "Y" : "N", "Loyalty Enrollment", "24"),
+                    new Subscription(time, b ? "Y" : "N", "Loyalty Communication", "25")};
+
+            preferences = new Preference[] {
+                    new Preference("de-DE", "de-DE", 1), new Preference("Y", "Y", 2),
+                    new Preference("Y", "Y", 3), new Preference("123456", "123456", 4),
+                    new Preference("Y", "Y", 6), new Preference("Y", "Y", 7),
+                    new Preference("Y", "Y", 8), new Preference("Y", "Y", 9),
+                    new Preference("Y", "Y", 10), new Preference(11),
+                    new Preference("Y", 12), new Preference("Y", 13),
+                    new Preference("Y", 14), new Preference("Y", 15),
+                    new Preference("Y", 16), new Preference("Y", 16),
+                    new Preference("Y", 17), new Preference("Y", 18),
+                    new Preference("Y", 19), new Preference("Y", 20),
+                    new Preference("Y", 21), new Preference("Y", 22),
+            };
 
             if(b) {
                 devices = new Device[] {new Device(deviceId)};
@@ -53,7 +80,7 @@ public class ProfileRequest implements Request {
         }
     }
 
-    public static class Subscription {
+    private static class Subscription {
         private final String optInStatus, subscriptionDesc, subscriptionId;
         private String optInDate, optOutDate;
 
@@ -74,32 +101,21 @@ public class ProfileRequest implements Request {
         private final String registrationChannel = "M";
     }
 
-    public static class Base {
+    private static class Base {
         private final String firstName = "M", lastName = "M";
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
     }
 
-    public static class Device {
-        private final String deviceId, pushNotificationId = "c9ZdzkqwfgY:APA91bE910Kbyi2japDOTgwPtLM7Sl3UEMg_XdmtjSBpwRTXo4FH-_2pCtVzlbvbNPMNkRJN41B-5LZnK7ZDGH3namwOAXrjh-rhfVfSTXkh9DdFCZv99Joew7LJKokiUiUFuwdKZhnV";
+    private static class Device {
+        private final String deviceId, isActive = "Y", timezone = "Europe/Berlin",
+                pushNotificationId = "c9ZdzkqwfgY:APA91bE910Kbyi2japDOTgwPtLM7Sl3UEMg_XdmtjSBpwRTXo4FH-_2pCtVzlbvbNPMNkRJN41B-5LZnK7ZDGH3namwOAXrjh-rhfVfSTXkh9DdFCZv99Joew7LJKokiUiUFuwdKZhnV";
 
-        public Device(String deviceId) {
+        private Device(String deviceId) {
             this.deviceId = deviceId;
         }
     }
 
-    public static class Email {
-        private final String activeInd = "Y", emailAddress,  primaryInd = "Y", type = "personal", verifiedInd = "Y";
-
-        public Email(String emailAddress) {
-            this.emailAddress = emailAddress;
-        }
+    private static class Email {
+        private final String verifiedInd = "Y";
     }
 
     public static class Address {
@@ -137,6 +153,26 @@ public class ProfileRequest implements Request {
                     return zipCode;
                 }
             }
+        }
+    }
+
+    private static class Preference {
+        private final Map<String, String> details = new HashMap<>();
+        private final int preferenceId;
+
+        private Preference(String email, String mobileApp, int preferenceId) {
+            details.put("email", email);
+            details.put("mobileApp", mobileApp);
+            this.preferenceId = preferenceId;
+        }
+
+        private Preference(String enabled, int preferenceId) {
+            details.put("enabled", enabled);
+            this.preferenceId = preferenceId;
+        }
+
+        private Preference(int preferenceId) {
+            this.preferenceId = preferenceId;
         }
     }
 }
