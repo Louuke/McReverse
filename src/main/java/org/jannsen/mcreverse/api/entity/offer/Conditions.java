29da9376206e04cadb09236f627b71fda228a584
JicuNull <69324615+JicuNull@users.noreply.github.com>
2022-11-04 20:25:48 +0100

    Fix type of saleAmountConditions

--- src/main/java/org/jannsen/mcreverse/api/entity/offer/Conditions.java
+++ src/main/java/org/jannsen/mcreverse/api/entity/offer/Conditions.java
@@ -1,21 +1,51 @@
 package org.jannsen.mcreverse.api.entity.offer;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Conditions {
 
-    private List<String> dateConditions, dayOfWeekConditions, saleAmountConditions;
+    @SerializedName("dateConditions") private List<String> dateConditions;
+    @SerializedName("dayOfWeekConditions") private List<Object> dayOfWeekConditions;
+    @SerializedName("saleAmountConditions") private List<SaleAmountConditions> saleAmountConditions;
 
     public List<String> getDateConditions() {
         return dateConditions != null ? dateConditions : new ArrayList<>();
     }
 
-    public List<String> getDayOfWeekConditions() {
+    public List<Object> getDayOfWeekConditions() {
         return dayOfWeekConditions != null ? dayOfWeekConditions : new ArrayList<>();
     }
 
-    public List<String> getSaleAmountConditions() {
+    public List<SaleAmountConditions> getSaleAmountConditions() {
         return saleAmountConditions != null ? saleAmountConditions : new ArrayList<>();
+    }
+
+    public static class SaleAmountConditions {
+
+        private boolean includeEligible, preTaxValidation, includeNonProduct, includeGiftCoupons;
+        private int minimum;
+
+        public boolean isIncludeEligible() {
+            return includeEligible;
+        }
+
+        public boolean isIncludeGiftCoupons() {
+            return includeGiftCoupons;
+        }
+
+        public boolean isIncludeNonProduct() {
+            return includeNonProduct;
+        }
+
+        public boolean isPreTaxValidation() {
+            return preTaxValidation;
+        }
+
+        public int getMinimum() {
+            return minimum;
+        }
     }
 }
