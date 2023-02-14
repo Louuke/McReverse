550e625e5b02d5fc9fba25288667e35a7368c8b6
JicuNull <69324615+JicuNull@users.noreply.github.com>
2023-02-14 21:50:32 +0100

    Fix NoSuchFieldException

--- src/test/java/org/jannsen/mcreverse/SensorFetcher.java
+++ src/test/java/org/jannsen/mcreverse/SensorFetcher.java
@@ -1,11 +1,35 @@
 package org.jannsen.mcreverse;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.jannsen.mcreverse.api.entity.akamai.SensorToken;
 
-public class SensorFetcher {
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.function.Supplier;
 
-    public static SensorToken queryToken() {
-        // QUERY TOKEN
-        return null;
+public class SensorFetcher implements Supplier<SensorToken> {
+
+    @Override
+    public SensorToken get() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.mccoupon.deals/token"))
+                    .GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String body = response.body();
+            JsonElement element = JsonParser.parseString(body);
+            String t = element.getAsJsonObject().get("response").getAsJsonObject().get("token").getAsString();
+            long l = element.getAsJsonObject().get("response").getAsJsonObject().get("createdTime").getAsLong();
+            return new SensorToken(t, l);
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
