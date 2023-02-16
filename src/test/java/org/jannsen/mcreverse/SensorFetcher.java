45de231b0fd42d8a49b6099deb91534c5a661b5e
JicuNull <69324615+JicuNull@users.noreply.github.com>
2023-02-16 08:35:26 +0100

    Update SensorFetcher.java

--- src/test/java/org/jannsen/mcreverse/SensorFetcher.java
+++ src/test/java/org/jannsen/mcreverse/SensorFetcher.java
@@ -1,35 +1,14 @@
 package org.jannsen.mcreverse;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import org.jannsen.mcreverse.api.entity.akamai.SensorToken;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.function.Supplier;
 
 public class SensorFetcher implements Supplier<SensorToken> {
 
     @Override
     public SensorToken get() {
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("https://api.mccoupon.deals/token"))
-                    .GET()
-                    .build();
-            HttpClient client = HttpClient.newHttpClient();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String body = response.body();
-            JsonElement element = JsonParser.parseString(body);
-            String t = element.getAsJsonObject().get("response").getAsJsonObject().get("token").getAsString();
-            long l = element.getAsJsonObject().get("response").getAsJsonObject().get("createdTime").getAsLong();
-            return new SensorToken(t, l);
-        } catch (URISyntaxException | InterruptedException | IOException e) {
-            throw new RuntimeException(e);
-        }
+        // QUERY TOKEN
+        return new SensorToken();
     }
 }
